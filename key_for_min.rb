fa3923dc517e89ddd412d84c0d793d3157296254
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-23 16:08:41 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,12 @@ def key_for_min_value(my_hash)
     lowest_value = nil
     lowest_key = nil
     my_hash.each do |key, value|
-      if value < lowest_value || lowest_value == nil
+      if lowest_value == nil || value < lowest_value  
           lowest_value = value
           lowest_key = key
       end
     end
     lowest_key
 end
+
+key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})
