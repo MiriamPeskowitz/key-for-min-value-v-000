9d30013a82a16747f3f69840130a42e7f93039b3
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-23 16:02:01 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,10 @@ def key_for_min_value(hash)
     lowest_value= nil
     lowest_key = nil
     hash.each do |key, value|
-        if value < lowest_value
+        if value < lowest_value || lowest_value == nil
           lowest_value = value
           lowest_key = key
         end
       end
     lowest_key
 end
-# cant sort over a hash. turn into an array, sort, then turn it back to a hask?
