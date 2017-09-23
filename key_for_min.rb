43fe368735a02605601f23adbd372d54151b25f4
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-23 16:00:37 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@ def key_for_min_value(hash)
     lowest_value= nil
     lowest_key = nil
     hash.each do |key, value|
-        if lowest_value > value
+        if value < lowest_value
           lowest_value = value
           lowest_key = key
         end
