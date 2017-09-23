d0fc6e55e001f5973a90d8eeffe064927193b2d1
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-23 15:27:40 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
 
+
+def key_for_min_value(hash)
+    #hash.sort_by { |key, value| [-value, key] }
+  # is value greater or less than the value that preceded it?
+    hash.each { |key, value| value.sortirb
 end
+# cant sort over a hash. turn into an array, sort, then turn it back to a hask?
+key_for_min_value(['a'=>'1', 'c'=>'3', 'b'=>'2'])
